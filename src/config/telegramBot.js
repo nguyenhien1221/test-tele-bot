a75e9737c8f5821fdc69c5b715e738a5c851f03b
Teehee <nguyenhien2942000@gmail.com>
2024-04-21 23:52:38 +0700

    update: env viriable

--- src/config/telegramBot.js
+++ src/config/telegramBot.js
@@ -4,7 +4,7 @@ const TOKEN = "6949670904:AAGRsfNNWB-9EAx99M6s6onm0iCma4fAPtU";
 
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://main--tele-bot-test.netlify.app/";
+const web_link = process.env.REACT_APP_WEB_URL;
 
 bot.start((ctx) => {
   ctx.reply("Welcome", {
