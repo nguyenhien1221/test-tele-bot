85fc1a747059f6d3393f84a9e6a0140387bca994
hiennguyen2904 <hien.nguyen@icetea.io>
2024-04-22 18:38:34 +0700

    fix: UI

--- src/config/telegramBot.js
+++ src/config/telegramBot.js
@@ -44,28 +44,47 @@ bot.start(async (ctx) => {
 });
 
 //message when open box chat
-bot.start(async (ctx) => {
-  const imagePath = path.join(__dirname, "../../public/images/about.png");
-  const imageBuffer = fs.readFileSync(imagePath);
-  const imageStream = imageBuffer;
+// bot.start(async (ctx) => {
+//   const imagePath = path.join(__dirname, "../../public/images/about.png");
+//   const imageBuffer = fs.readFileSync(imagePath);
+//   const imageStream = imageBuffer;
 
-  const caption = `
-    <b>What can this bot do?</b>
-    New generation Telegram wallet, built on NEAR
-    Description: HERE is a Telegram-based non-custodial wallet 
-    designed for the NEAR Protocol. Easily import or create an account,
-    transfer tokens, and mine HOT. This wallet operates independently of your login account,
-    ensuring all data is securely stored locally on your device
-    
-    Support: @herewalletchat
-    HOT Coin News: @hotnear
-    Open Wallet: https://wallet.here.app
-  `;
+//   const caption = `
+//     <b>What can this bot do?</b>
+//     New generation Telegram wallet, built on NEAR
+//     Description: HERE is a Telegram-based non-custodial wallet
+//     designed for the NEAR Protocol. Easily import or create an account,
+//     transfer tokens, and mine HOT. This wallet operates independently of your login account,
+//     ensuring all data is securely stored locally on your device
 
-  await ctx.replyWithPhoto(
-    { source: imageStream },
-    { caption, parse_mode: "HTML" }
-  );
+//     Support: @herewalletchat
+//     HOT Coin News: @hotnear
+//     Open Wallet: https://wallet.here.app
+//   `;
+
+//   await ctx.replyWithPhoto(
+//     { source: imageStream },
+//     { caption, parse_mode: "HTML" }
+//   );
+// });
+
+bot.on("web_app_data", (ctx) => {
+  const data = ctx.webAppData;
+
+  if (data.data) {
+    // User opened an external link
+    const openedLink = data.data;
+    console.log("User opened link:", openedLink);
+
+    // You can handle the opened link here
+    // ...
+  } else {
+    // User interacted with the WebApp
+    console.log("User interacted with the WebApp");
+
+    // You can handle the WebApp interaction here
+    // ...
+  }
 });
 
 bot.launch();
