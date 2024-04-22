d7422e5ed33378b77d5eb2a5f2cb120cd2d123ba
Teehee <nguyenhien2942000@gmail.com>
2024-04-22 23:04:28 +0700

    update: env viriable

--- src/config/telegramBot.js
+++ src/config/telegramBot.js
@@ -1,4 +1,4 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf,  } = require("telegraf");
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
@@ -12,18 +12,7 @@ bot.start(async (ctx) => {
   const imageBuffer = fs.readFileSync(imagePath);
   const imageStream = imageBuffer;
 
-  const caption = `
-    <b>What can this bot do?</b>
-    New generation Telegram wallet, built on NEAR
-    Description: HERE is a Telegram-based non-custodial wallet 
-    designed for the NEAR Protocol. Easily import or create an account,
-    transfer tokens, and mine HOT. This wallet operates independently of your login account,
-    ensuring all data is securely stored locally on your device
-
-    Support: @herewalletchat
-    HOT Coin News: @hotnear
-    Open Wallet: https://wallet.here.app
-  `;
+  const caption ='<i>New generation Telegram wallet</i>, built on NEAR Description: HERE is a Telegram-based non-custodial wallet designed for the NEAR Protocol. Easily import or create an account, transfer tokens, and mine HOT. This wallet operates independently of your login account, ensuring all data is securely stored locally on your device';
 
   await ctx.replyWithPhoto(
     { source: imageStream },
@@ -33,58 +22,17 @@ bot.start(async (ctx) => {
       reply_markup: {
         inline_keyboard: [
           [
-            { text: "Join the community", url: "t.me/seed_coin_bot" },
+            { text: "Join the community", url: "t.me/Catia_Announcement" },
             // { text: "Learn more about SEED coin", url: "" },
             { text: "Open Wallet", web_app: { url: web_link } },
           ],
         ],
-      },
+      }
     }
   );
+
+  
 });
 
-//message when open box chat
-// bot.start(async (ctx) => {
-//   const imagePath = path.join(__dirname, "../../public/images/about.png");
-//   const imageBuffer = fs.readFileSync(imagePath);
-//   const imageStream = imageBuffer;
-
-//   const caption = `
-//     <b>What can this bot do?</b>
-//     New generation Telegram wallet, built on NEAR
-//     Description: HERE is a Telegram-based non-custodial wallet
-//     designed for the NEAR Protocol. Easily import or create an account,
-//     transfer tokens, and mine HOT. This wallet operates independently of your login account,
-//     ensuring all data is securely stored locally on your device
-
-//     Support: @herewalletchat
-//     HOT Coin News: @hotnear
-//     Open Wallet: https://wallet.here.app
-//   `;
-
-//   await ctx.replyWithPhoto(
-//     { source: imageStream },
-//     { caption, parse_mode: "HTML" }
-//   );
-// });
-
-bot.on("web_app_data", (ctx) => {
-  const data = ctx.webAppData;
-
-  if (data.data) {
-    // User opened an external link
-    const openedLink = data.data;
-    console.log("User opened link:", openedLink);
-
-    // You can handle the opened link here
-    // ...
-  } else {
-    // User interacted with the WebApp
-    console.log("User interacted with the WebApp");
-
-    // You can handle the WebApp interaction here
-    // ...
-  }
-});
 
 bot.launch();
