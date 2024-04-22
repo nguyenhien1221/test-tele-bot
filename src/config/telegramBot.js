8378cbe8afc1dc1ad0536484d0d0389a9dfb9959
hiennguyen2904 <hien.nguyen@icetea.io>
2024-04-22 15:58:23 +0700

    fix: bot

--- src/config/telegramBot.js
+++ src/config/telegramBot.js
@@ -1,22 +1,65 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 
 const TOKEN = process.env.REACT_APP_BOT_TOKEN;
-
 const bot = new Telegraf(TOKEN);
-
 const web_link = process.env.REACT_APP_WEB_URL;
 
-bot.start((ctx) => {
-  ctx.replyWithHTML("wwelcomeeeee", {
-    reply_markup: {
-      inline_keyboard: [[{ text: "Open app", web_app: { url: web_link } }]],
-    },
-  });
+bot.start(async (ctx) => {
+  const imagePath = path.join(__dirname, "../../public/images/about.png");
+  const imageBuffer = fs.readFileSync(imagePath);
+  const imageStream = imageBuffer;
+
+  const caption = `
+    <b>What can this bot do?</b>
+    New generation Telegram wallet, built on NEAR
+    Description: HERE is a Telegram-based non-custodial wallet 
+    designed for the NEAR Protocol. Easily import or create an account,
+    transfer tokens, and mine HOT. This wallet operates independently of your login account,
+    ensuring all data is securely stored locally on your device
+
+    Support: @herewalletchat
+    HOT Coin News: @hotnear
+    Open Wallet: https://wallet.here.app
+  `;
+
+  await ctx.replyWithPhoto(
+    { source: imageStream },
+    {
+      caption,
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [[{ text: "Open app", web_app: { url: web_link } }]],
+      },
+    }
+  );
 });
 
-bot.on((ctx) => {
-  ctx.replyWithHTML("tesstttt");
+//message when open box chat
+bot.on(async (ctx) => {
+  const imagePath = path.join(__dirname, "../../public/images/about.png");
+  const imageBuffer = fs.readFileSync(imagePath);
+  const imageStream = imageBuffer;
+
+  const caption = `
+    <b>What can this bot do?</b>
+    New generation Telegram wallet, built on NEAR
+    Description: HERE is a Telegram-based non-custodial wallet 
+    designed for the NEAR Protocol. Easily import or create an account,
+    transfer tokens, and mine HOT. This wallet operates independently of your login account,
+    ensuring all data is securely stored locally on your device
+    
+    Support: @herewalletchat
+    HOT Coin News: @hotnear
+    Open Wallet: https://wallet.here.app
+  `;
+
+  await ctx.replyWithPhoto(
+    { source: imageStream },
+    { caption, parse_mode: "HTML" }
+  );
 });
 
 bot.launch();
