a8882cea8a74ba3436e530469b8acfda141970dd
hiennguyen2904 <hien.nguyen@icetea.io>
2024-04-26 09:54:11 +0700

    fix: do mission

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const tele = window.Telegram.WebApp;
+// const tele = window.Telegram.WebApp;
 
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
