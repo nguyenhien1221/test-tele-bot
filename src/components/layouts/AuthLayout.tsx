fc1c51830a9daae7a88d7b59297a23d85511678d
Teehee <nguyenhien2942000@gmail.com>
2024-05-10 00:34:27 +0700

    fix: shared link

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -10,6 +10,13 @@ const AuthLayout = () => {
   tele.setHeaderColor("#F7FFEB");
   tele.expand();
 
+  const overflow = 100
+document.body.style.overflowY = 'hidden'
+document.body.style.marginTop = `${overflow}px`
+document.body.style.height = window.innerHeight + overflow + "px"
+document.body.style.paddingBottom = `${overflow}px`
+window.scrollTo(0, overflow)
+
   const AcountData = useGetAcountDetails();
 
   if (AcountData.isLoading) return <Loading />;
