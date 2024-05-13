d61e8854efcb937e2b9cb239039906734415187f
hiennguyen2904 <hien.nguyen@icetea.io>
2024-05-13 17:23:48 +0700

    fix: bug

--- src/modules/Boots/Components/WaterMissionModal.tsx
+++ src/modules/Boots/Components/WaterMissionModal.tsx
@@ -61,57 +61,67 @@ const WaterMissionsModal = ({
             <Loading />
           ) : (
             <div className="pt-[20px] max-h-[340px] flex-1 overflow-auto">
-              {data?.map((item: any, index: number) => {
-                return (
-                  <button
-                    disabled={isLoading}
-                    key={item.id}
-                    onClick={() => {
-                      handleSubmitMission(item);
-                    }}
-                    rel="noreferrer"
-                    className={clsx("text-center relative w-full")}
-                  >
-                    {item.task_user != null && (
+              {data
+                ?.sort((a: any, b: any) => {
+                  if (a.task_user === null && b.task_user !== null) {
+                    return -1; // a comes first if it has null value
+                  } else if (a.task_user !== null && b.task_user === null) {
+                    return 1; // b comes first if it has null value
+                  } else {
+                    return 0; // otherwise, maintain current order
+                  }
+                })
+                .map((item: any, index: number) => {
+                  return (
+                    <button
+                      disabled={isLoading}
+                      key={item.id}
+                      onClick={() => {
+                        handleSubmitMission(item);
+                      }}
+                      rel="noreferrer"
+                      className={clsx("text-center relative w-full")}
+                    >
+                      {item.task_user != null && (
+                        <div
+                          className={clsx(
+                            "w-[30px] h-[30px] rounded-[50%] flex items-center justify-center absolute right-4 -top-4 z-20",
+                            "border-[3px] border-[#B0D381] border-solid drop-shadow-[0_4px_0px_#4D7F0C] bg-[#7BB52C]"
+                          )}
+                        >
+                          <img
+                            src="/images/icons/checkmission.png"
+                            className="w-[13px] h-[9px]"
+                            alt=""
+                          ></img>
+                        </div>
+                      )}
                       <div
+                        key={index}
                         className={clsx(
-                          "w-[30px] h-[30px] rounded-[50%] flex items-center justify-center absolute right-4 -top-4 z-20",
-                          "border-[3px] border-[#B0D381] border-solid drop-shadow-[0_4px_0px_#4D7F0C] bg-[#7BB52C]"
+                          "z-10 py-3 px-4 relative cursor-pointer grid grid-cols-12 gap-3 bg-white rounded-2xl p-4 w-full mb-[18px] ",
+                          "dark:gradient-border-mask-mission dark:bg-transparent",
+                          "dark:boder-0 dark:drop-shadow-none dark:border-transparent",
+                          item.task_user
+                            ? "border-[1px] border-solid border-[#000] drop-shadow-none brightness-50"
+                            : "border-[3px] border-[#97C35B] border-solid drop-shadow-[0_4px_0px_#4D7F0C]"
                         )}
                       >
-                        <img
-                          src="/images/icons/checkmission.png"
-                          className="w-[13px] h-[9px]"
-                          alt=""
-                        ></img>
-                      </div>
-                    )}
-                    <div
-                      key={index}
-                      className={clsx(
-                        "z-10 py-3 px-4 relative cursor-pointer grid grid-cols-12 gap-3 bg-white rounded-2xl p-4 w-full mb-[18px] ",
-                        "dark:gradient-border-mask-mission dark:bg-transparent",
-                        "dark:boder-0 dark:drop-shadow-none dark:border-transparent",
-                        item.task_user
-                          ? "border-[1px] border-solid border-[#000] drop-shadow-none brightness-50"
-                          : "border-[3px] border-[#97C35B] border-solid drop-shadow-[0_4px_0px_#4D7F0C]"
-                      )}
-                    >
-                      <div className="col-span-2 flex items-center">
-                        <img
-                          src={`/images/holy/${item.type}.png`}
-                          className="w-8 h-8"
-                          alt=""
-                        ></img>
-                      </div>
+                        <div className="col-span-2 flex items-center">
+                          <img
+                            src={`/images/holy/${item.type}.png`}
+                            className="w-8 h-8"
+                            alt=""
+                          ></img>
+                        </div>
 
-                      <div className="col-span-8 flex items-center justify-start text-start text-[15px]">
-                        {item.name}
+                        <div className="col-span-8 flex items-center justify-start text-start text-[15px]">
+                          {item.name}
+                        </div>
                       </div>
-                    </div>
-                  </button>
-                );
-              })}
+                    </button>
+                  );
+                })}
 
               <div
                 className={clsx(
