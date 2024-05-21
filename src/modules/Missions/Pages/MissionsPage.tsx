19339cdfea25007308eeb76533eff28df6fd6a78
hiennguyen2904 <hien.nguyen@icetea.io>
2024-05-21 16:24:22 +0700

    fix: app

--- src/modules/Missions/Pages/MissionsPage.tsx
+++ src/modules/Missions/Pages/MissionsPage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { missionsTypes } from "../../../constants/missions.constants";
 import MissionsModal from "../Components/MissionsModal";
 import { useLocation, useNavigate } from "react-router-dom";
