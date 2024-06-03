e5b6ac7e0758beeb4f6af4547e376335e60f5d91
hiennguyen2904 <hien.nguyen@icetea.io>
2024-06-03 10:28:26 +0700

    update: share link

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -14,12 +14,12 @@ const AuthLayout = () => {
   }
   tele.expand();
 
-  const overflow = 100;
-  document.body.style.overflowY = "hidden";
-  document.body.style.marginTop = `${overflow}px`;
-  document.body.style.height = window.innerHeight + overflow + "px";
-  document.body.style.paddingBottom = `${overflow}px`;
-  window.scrollTo(0, overflow);
+  // const overflow = 100;
+  // document.body.style.overflowY = "auto";
+  // document.body.style.marginTop = `${overflow}px`;
+  // document.body.style.height = window.innerHeight + overflow + "px";
+  // document.body.style.paddingBottom = `${overflow}px`;
+  // window.scrollTo(0, overflow);
 
   const AcountData = useGetAcountDetails();
 
